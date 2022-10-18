681334cf15e89ac5ee1271442784bd179c868fbb
ludovicjj <jahanlud@gmail.com>
2022-10-18 13:29:31 +0200

    BlogController: Add action ::new() to create manually new contributor

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,15 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\Contributor;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
-    #[Route('/', name: 'app_bloch_home')]
-    public function index(): Response
+    #[Route('/', name: 'app_blog_home')]
+    public function index(UserRepository $userRepository): Response
     {
-        return $this->render('posts/index.html.twig', []);
+        $users = $userRepository->findAll();
+        return $this->render('posts/index.html.twig', [
+            'users' => $users
+        ]);
+    }
+
+    #[Route('/new/data')]
+    public function new(EntityManagerInterface $entityManager): Response
+    {
+        // Will be replaced by fixtures soon
+        $user = new Contributor();
+        $user
+            ->setFirstname('John')
+            ->setLastname('Doe')
+            ->setEmail('johndoe@email.com')
+            ->setPassword("123456") // use UserPasswordHasherInterface to hash plainPassword
+        ;
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return new Response(sprintf(
+            'Contributor %d created with email %s',
+            $user->getId(),
+            $user->getEmail()
+        ));
     }
 }
