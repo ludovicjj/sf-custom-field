250074ca3b5cc4fc1af92d08a253b28e698a2e6d
ludovicjj <jahanlud@gmail.com>
2022-10-18 16:42:27 +0200

    Init Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\PostFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,7 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        PostFactory::createMany(10);
         $manager->flush();
     }
 }
