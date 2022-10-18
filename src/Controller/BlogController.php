842aa3fb154b035b4fc1a7daa4d5f7614230715c
ludovicjj <jahanlud@gmail.com>
2022-10-18 16:15:57 +0200

    BlogController: Remove action ::new()

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,9 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Contributor;
 use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,26 +16,5 @@ class BlogController extends AbstractController
         return $this->render('posts/index.html.twig', [
             'users' => $users
         ]);
-    }
-
-    #[Route('/new/data')]
-    public function new(EntityManagerInterface $entityManager): Response
-    {
-        // Will be replaced by fixtures soon
-        $user = new Contributor();
-        $user
-            ->setFirstname('John')
-            ->setLastname('Doe')
-            ->setEmail('johndoe@email.com')
-            ->setPassword("123456") // use UserPasswordHasherInterface to hash plainPassword
-        ;
-        $entityManager->persist($user);
-        $entityManager->flush();
-
-        return new Response(sprintf(
-            'Contributor %d created with email %s',
-            $user->getId(),
-            $user->getEmail()
-        ));
     }
 }
