1038606ea1f3caff0b6084f727011807402a7647
ludovicjj <jahanlud@gmail.com>
2022-10-18 16:02:21 +0200

    Require foundry

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
