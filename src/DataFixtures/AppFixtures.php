9e7e6e6cc2c3f4c5ee9ac36440a127763a9b6956
ludovicjj <jahanlud@gmail.com>
2022-10-18 17:26:51 +0200

    Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Factory\PostFactory;
+use App\Factory\TagFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,7 +11,16 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        PostFactory::createMany(10);
+        // Pre-create Tags
+        TagFactory::createMany(10);
+
+        PostFactory::new()
+            ->many(5) // Create 5 posts
+            ->create(function() {
+                // Each post uses between 0 and 5 random tags from those already in the database
+                return ['tags' => TagFactory::randomRange(1, 5)];
+            });
+
         $manager->flush();
     }
 }
