d43ca1b4540cc3a737b3f317fbfe405b180bc5a5
ludovicjj <jahanlud@gmail.com>
2022-10-20 15:44:41 +0200

    Define option search value to url to route api_tag_search

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -7,16 +7,22 @@ use App\Entity\Tag;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class PostType extends AbstractType
 {
+    public function __construct(private UrlGeneratorInterface $urlGenerator)
+    {
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('title')
             ->add('content')
             ->add('tags', TomSelectType::class, [
-                'class' => Tag::class
+                'class' => Tag::class,
+                'search' => $this->urlGenerator->generate('api_tag_search')
             ])
         ;
     }
